d416e565ed654917bd73dcdb5079cf9ce0299b97
Tyrone Jher Tongol <tongoltyrone84@gmail.com>
2026-05-15 15:58:38 +1000

    fix: explicitly set Authorization header in admin client

--- apps/web/src/lib/supabase/admin.ts
+++ apps/web/src/lib/supabase/admin.ts
@@ -1,13 +1,19 @@
 import { createClient } from '@supabase/supabase-js';
 
 export function createAdminClient() {
+  const key = process.env['SUPABASE_SERVICE_ROLE_KEY']!;
   return createClient(
     process.env['NEXT_PUBLIC_SUPABASE_URL']!,
-    process.env['SUPABASE_SERVICE_ROLE_KEY']!,
+    key,
     {
       auth: {
         autoRefreshToken: false,
         persistSession: false,
+      },
+      global: {
+        headers: {
+          Authorization: `Bearer ${key}`,
+        },
       },
     },
   );
